451b3f74f761eca8ae0d3fe48e4fbb77b9fda66f
Icaro Weiler <icarog28@gmail.com>
2022-07-01 06:53:31 -0300

    feat(footer): adiciona botoes no footer

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GamesListComponent } from './components/games-list/games-list.component';
 import {CarouselModule} from 'primeng/carousel'
+import { ButtonModule } from 'primeng/button';
 
 
 
@@ -13,7 +14,8 @@ import {CarouselModule} from 'primeng/carousel'
   ],
   imports: [
     CommonModule,
-    CarouselModule
+    CarouselModule,
+    ButtonModule
   ]
 })
 export class GamesModule { }
