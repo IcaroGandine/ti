beab89f9c94412cbfec0b017e14847852e870ef8
Icaro Weiler <icarog28@gmail.com>
2022-07-01 04:36:33 -0300

    feat(games-list): adiciona estrutura para lista de games, header e footer padrão

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
+import { GamesListComponent } from './games/components/games-list/games-list.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: GamesListComponent },
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
